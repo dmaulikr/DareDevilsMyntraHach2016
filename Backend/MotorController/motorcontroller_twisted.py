f56680d25ab2f45f4c9dc39aaf12b05c7126c399
Himadri Jyoti <himadri.jyoti@urbanladder.com>
2016-04-16 13:03:42 +0530

    Disable warning for concurrent I/O

--- Backend/MotorController/motorcontroller_twisted.py
+++ Backend/MotorController/motorcontroller_twisted.py
@@ -4,10 +4,14 @@ from twisted.internet import reactor
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
+GPIO.setwarnings(False)
+
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 GPIO.setup(23, GPIO.OUT)
+
+
 
 class MotorController(Protocol):
     def connectionMade(self):
@@ -24,7 +28,8 @@ factory.protocol = MotorController
 factory.clients = []
 
 reactor.listenTCP(6666, factory)
+print "MotorController server starting..."
 reactor.run()
-print "MotorController server started"
+
 
 
