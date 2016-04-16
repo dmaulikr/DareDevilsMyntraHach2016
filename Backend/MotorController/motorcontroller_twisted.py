f467d9a90211de51d81f9c5ad7f7545af058d2b0
Himadri Jyoti <himadri.jyoti@urbanladder.com>
2016-04-16 12:56:58 +0530

    Basic protocol for Factory of motor controller

--- Backend/MotorController/motorcontroller_twisted.py
+++ Backend/MotorController/motorcontroller_twisted.py
@@ -6,4 +6,22 @@ GPIO.setup(18, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 GPIO.setup(23, GPIO.OUT)
 
+class MotorController(Protocol):
+    def connectionMade(self):
+        print "Connection established"
+    
+    def connectionLost(self, reason):
+        print "Connection lost"
+
+
+
+# Creation code
+factory = Factory()
+factory.protocol = MotorController
+factory.clients = []
+
+reactor.listenTCP(6666, factory)
+reactor.run()
+print "MotorController server started"
+
 
