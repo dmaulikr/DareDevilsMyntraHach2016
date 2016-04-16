c97d3d2b3b0610e93d13980740dfe7b30827d53a
Himadri Jyoti <himadri.jyoti@urbanladder.com>
2016-04-16 12:23:03 +0530

    Skeleton iPad app, basic folder structure

---
+++ iPadApp/AutoInventory/AutoInventory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoInventory
+//
+//  Created by Himadri Jyoti on 16/04/16.
+//  Copyright © 2016 Himadri Jyoti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iPadApp/AutoInventory/AutoInventory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoInventory
+//
+//  Created by Himadri Jyoti on 16/04/16.
+//  Copyright © 2016 Himadri Jyoti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
